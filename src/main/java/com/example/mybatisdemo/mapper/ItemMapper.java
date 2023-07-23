463c18fa4fe2ca3d3a52895d86a1ed08fe61b138
nas <vividnasubi@gmail.com>
2023-07-23 17:44:56 +0900

    feat: APIのRead機能を追加

--- src/main/java/com/example/mybatisdemo/mapper/ItemMapper.java
+++ src/main/java/com/example/mybatisdemo/mapper/ItemMapper.java
@@ -3,7 +3,10 @@ package com.example.mybatisdemo.mapper;
 import com.example.mybatisdemo.entity.Item;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface ItemMapper {
   Item findById(int id);
+  List<Item> findAll();
 }
