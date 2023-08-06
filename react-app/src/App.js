eb8a2377a29fd2ddf72a967f33622000f0e6e28a
nas <vividnasubi@gmail.com>
2023-08-06 17:07:57 +0900

    feat: フロントからのAPIの呼び出しを実装

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,9 +1,31 @@
+import { useEffect, useState } from "react";
+
 const App = () => {
+  const url = 'http://localhost:8080/items';
+  const [items, setItems] = useState([]);
+  
+  useEffect(() => {
+    fetch(url,{method: 'GET'})
+    .then(res => res.json())
+    .then(data => {
+      setItems(data)
+    })
+  },[]);
+
   return (
-    <div className="App">
-      Hello, world!
-    </div>
+    <>
+      <div className="App">
+        <p>{console.log(items)}</p>
+        <ul>
+          {
+            items.map(item => 
+              <li key={item.id}>{item.itemName}</li>
+            )
+          }
+        </ul>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
